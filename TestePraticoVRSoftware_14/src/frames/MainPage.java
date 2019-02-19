e783589d515288bbe8bb531ebb2585bd133fada9
Agostinho <agostinho.guimaraes@fatec.sp.gov.br>
2019-02-19 00:31:37 -0300

    Concertando

---
+++ TestePraticoVRSoftware_14/src/frames/MainPage.java
@@ -0,0 +1,216 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frames;
+
+import classes.Aluno;
+import classes.Curso_Aluno;
+import dal.Conectadb;
+import dal.CreateDB;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author tinho
+ */
+public class MainPage extends javax.swing.JFrame {
+
+  Connection con = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    ArrayList<Curso_Aluno> listaCurso_alunos = new ArrayList<Curso_Aluno>();
+    public MainPage() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+        MatriculasCadastrados();
+    }
+
+public void MatriculasCadastrados() {
+
+        try {
+            con = Conectadb.conectadb();
+
+            String scriptSQL = "Select * FROM curso_aluno";
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(scriptSQL);
+
+            int codigo;
+            int codigo_curso;
+            int codigo_aluno;
+            DefaultTableModel tabelaMatricula = (DefaultTableModel) matriculasTable.getModel();
+            while (rs.next()) {
+
+                codigo = rs.getInt("codigo");
+                codigo_curso = rs.getInt("codigo_curso");
+                codigo_aluno = rs.getInt("codigo_aluno");
+
+                Curso_Aluno curso_aluno = new Curso_Aluno(codigo, codigo_curso,codigo_aluno);
+                listaCurso_alunos.add(curso_aluno);
+                Object[] lista = {codigo,codigo_aluno, codigo_curso};
+                tabelaMatricula.addRow(lista);
+            }
+
+            rs.close();
+            con.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CadastrarAlunos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CadastrarAlunos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        matriculaAlunojButton = new javax.swing.JButton();
+        matriculaCursojButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        matriculasTable = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        matriculaAlunojButton.setText("Cadastro de Aluno");
+        matriculaAlunojButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                matriculaAlunojButtonActionPerformed(evt);
+            }
+        });
+
+        matriculaCursojButton.setText("Cadastro de Curso");
+        matriculaCursojButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                matriculaCursojButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Matrículas Realizadas");
+
+        matriculasTable.setAutoCreateRowSorter(true);
+        matriculasTable.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        matriculasTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Código", "Código_Aluno", "Código_Curso"
+            }
+        ));
+        jScrollPane1.setViewportView(matriculasTable);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(matriculaCursojButton)
+                    .addComponent(matriculaAlunojButton)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 220, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 870, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(matriculaAlunojButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(matriculaCursojButton)
+                .addGap(42, 42, 42)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(49, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void matriculaAlunojButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_matriculaAlunojButtonActionPerformed
+        try {
+            new CadastrarAlunos().setVisible(true);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MainPage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_matriculaAlunojButtonActionPerformed
+
+    private void matriculaCursojButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_matriculaCursojButtonActionPerformed
+        new CadastrarCursos().setVisible(true);
+    }//GEN-LAST:event_matriculaCursojButtonActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainPage().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton matriculaAlunojButton;
+    private javax.swing.JButton matriculaCursojButton;
+    private javax.swing.JTable matriculasTable;
+    // End of variables declaration//GEN-END:variables
+}
